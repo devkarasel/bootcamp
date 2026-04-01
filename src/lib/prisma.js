ffbc6bcef65bcd36d8ebe589b1f54559f5f17a41
rasel <devkarasel@gmail.com>
2026-04-01 16:53:21 +0600

    fix: deployment

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,5 +1,6 @@
-import { default as PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+
+const { PrismaClient } = await import('@prisma/client')
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
